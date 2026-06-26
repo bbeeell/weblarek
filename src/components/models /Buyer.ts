9d842a4429a1a6eecfe4c588d189c4c068417e82
why_bbbelll <borlakovabella326@gmail.com>
2026-06-26 23:45:25 +0300

    Buyer.ts

--- src/components/models /Buyer.ts
+++ src/components/models /Buyer.ts
@@ -1,12 +1,9 @@
 import { IBuyer, TPayment } from "../../types/index";
-import { IEvents } from "../base/Events";
 
 export class Buyer {
   protected data: IBuyer;
-  protected events: IEvents;
 
-  constructor(events: IEvents) {
-    this.events = events;
+  constructor() {
     this.data = {
       payment: "",
       address: "",
@@ -17,22 +14,18 @@ export class Buyer {
 
   savePaymentType(payment: TPayment) {
     this.data.payment = payment;
-    this.events.emit('buyer-data:changed', { field: 'payment' });
   }
 
   saveAddress(address: string) {
     this.data.address = address;
-    this.events.emit('buyer-data:changed', { field: 'address' });
   }
 
   saveEmail(email: string) {
     this.data.email = email;
-    this.events.emit('buyer-data:changed', { field: 'email' });
   }
 
   savePhone(phone: string) {
     this.data.phone = phone;
-    this.events.emit('buyer-data:changed', { field: 'phone' });
   }
 
   getData(): IBuyer {
