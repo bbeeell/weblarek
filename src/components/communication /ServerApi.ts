2416961863e8f281d591c48a373ede8b5c92d654
why_bbbelll <borlakovabella326@gmail.com>
2026-06-29 14:57:34 +0300

    ServerApi.ts

--- src/components/communication /ServerApi.ts
+++ src/components/communication /ServerApi.ts
@@ -1,49 +1,18 @@
-import { IProduct } from '../../types/index';
-import { IEvents } from '../base/Events';
+import { IApi } from '../base/Api';
+import { IOrderData, IOrderResponse, IProductList } from '../../types/index';
 
-export class ShoppingCart {
-  protected storedItems: IProduct[];
-  protected eventBus: IEvents;
+export class ServerApi {
+  protected requester: IApi;
 
-  constructor(events: IEvents) {
-    this.storedItems = [];
-    this.eventBus = events;
+  constructor(apiService: IApi) {
+    this.requester = apiService;
   }
 
-  public retrieveAllItems(): IProduct[] {
-    return this.storedItems;
-  }
-  
-  public pushItem(newItem: IProduct): void {
-    const isDuplicate = this.storedItems.some(item => item.id === newItem.id);
-    
-    if (!isDuplicate) {
-      this.storedItems.push(newItem);
-      this.eventBus.emit('cart:item-added');
-    }
+  public async requestProductCatalog(): Promise<IProductList> {
+    return this.requester.get('/product');
   }
 
-  public discardItem(targetId: string): void {
-    this.storedItems = this.storedItems.filter(item => item.id !== targetId);
-    this.eventBus.emit('cart:item-removed');
-  }
-
-  public resetCart(): void {
-    this.storedItems = [];
-    this.eventBus.emit('cart:cleared');
-  }
-
-  public evaluateTotalPrice(): number {
-    return this.storedItems.reduce((accumulator, currentProduct) => {
-      return accumulator + (currentProduct.price || 0);
-    }, 0);
-  }
-
-  public countItems(): number {
-    return this.storedItems.length;
-  }
-
-  public verifyItemExistence(itemId: string): boolean {
-    return this.storedItems.some(item => item.id === itemId);
+  public async submitOrder(payload: IOrderData): Promise<IOrderResponse> {
+    return this.requester.post('/order', payload);
   }
 }
