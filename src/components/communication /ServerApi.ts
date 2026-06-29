e4d6bcd6c4d009f4588f4cb5431ff3b43d79c547
why_bbbelll <borlakovabella326@gmail.com>
2026-06-29 14:56:46 +0300

    ServerApi.ts

--- src/components/communication /ServerApi.ts
+++ src/components/communication /ServerApi.ts
@@ -1,18 +1,49 @@
-import { IApi } from '../base/Api';
-import { IOrderData, IOrderResponse, IProductList } from '../../types/index';
+import { IProduct } from '../../types/index';
+import { IEvents } from '../base/Events';
 
-export class ServerApi {
-  protected requestHandler: IApi;
+export class ShoppingCart {
+  protected storedItems: IProduct[];
+  protected eventBus: IEvents;
 
-  constructor(apiInstance: IApi) {
-    this.requestHandler = apiInstance;
+  constructor(events: IEvents) {
+    this.storedItems = [];
+    this.eventBus = events;
   }
 
-  public async fetchGoods(): Promise<IProductList> {
-    return this.requestHandler.get('/product');
+  public retrieveAllItems(): IProduct[] {
+    return this.storedItems;
+  }
+  
+  public pushItem(newItem: IProduct): void {
+    const isDuplicate = this.storedItems.some(item => item.id === newItem.id);
+    
+    if (!isDuplicate) {
+      this.storedItems.push(newItem);
+      this.eventBus.emit('cart:item-added');
+    }
   }
 
-  public async placeNewOrder(orderDetails: IOrderData): Promise<IOrderResponse> {
-    return this.requestHandler.post('/order', orderDetails);
+  public discardItem(targetId: string): void {
+    this.storedItems = this.storedItems.filter(item => item.id !== targetId);
+    this.eventBus.emit('cart:item-removed');
+  }
+
+  public resetCart(): void {
+    this.storedItems = [];
+    this.eventBus.emit('cart:cleared');
+  }
+
+  public evaluateTotalPrice(): number {
+    return this.storedItems.reduce((accumulator, currentProduct) => {
+      return accumulator + (currentProduct.price || 0);
+    }, 0);
+  }
+
+  public countItems(): number {
+    return this.storedItems.length;
+  }
+
+  public verifyItemExistence(itemId: string): boolean {
+    return this.storedItems.some(item => item.id === itemId);
   }
 }
