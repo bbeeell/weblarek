a0931aa468aa0828a3fdfc195f3de5c9856b2b93
why_bbbelll <borlakovabella326@gmail.com>
2026-07-20 11:22:22 +0300

    Form.ts

--- src/components/view/Form.ts
+++ src/components/view/Form.ts
@@ -1,7 +1,7 @@
 import { Component } from '../base/Component';
 import { IEvents } from '../base/Events';
 
-export class Form<T> extends Component<T> {
+export abstract class Form<T> extends Component<T> {
     protected submitButton: HTMLButtonElement;
     protected errorsContainer: HTMLElement;
 
@@ -31,10 +31,13 @@ export class Form<T> extends Component<T> {
         this.setText(this.errorsContainer, value);
     }
 
-    render(state: Partial<T> & { errors?: string }): HTMLElement {
-        const { errors, ...inputs } = state;
-        if (errors) this.errors = errors;
-        Object.assign(this, inputs);
+    // render теперь стандартный. При вызове без аргументов он просто вернёт контейнер.
+    render(state?: Partial<T> & { errors?: string }): HTMLElement {
+        if (state) {
+            const { errors, ...inputs } = state;
+            if (errors) this.errors = errors;
+            Object.assign(this, inputs);
+        }
         return this.container;
     }
 }
