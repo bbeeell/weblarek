dba96a6baed5a5b3ce30b96fa3d3be174abcc74d
why_bbbelll <borlakovabella326@gmail.com>
2026-07-20 11:23:36 +0300

    ContactsForm.ts

--- src/components/view/ContactsForm.ts
+++ src/components/view/ContactsForm.ts
@@ -2,15 +2,20 @@ import { Form } from './Form';
 import { IContactsForm } from '../../types';
 
 export class ContactsForm extends Form<IContactsForm> {
+    protected emailInput: HTMLInputElement;
+    protected phoneInput: HTMLInputElement;
+
     constructor(container: HTMLFormElement, events: IEvents) {
         super(container, events);
+        this.emailInput = container.querySelector('input[name="email"]') as HTMLInputElement;
+        this.phoneInput = container.querySelector('input[name="phone"]') as HTMLInputElement;
     }
 
     set email(value: string) {
-        (this.container.querySelector('input[name="email"]') as HTMLInputElement).value = value;
+        this.emailInput.value = value;
     }
 
     set phone(value: string) {
-        (this.container.querySelector('input[name="phone"]') as HTMLInputElement).value = value;
+        this.phoneInput.value = value;
     }
 }
