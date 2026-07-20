9c637abc3ab33976c86094b0e452e28e3c032b94
why_bbbelll <borlakovabella326@gmail.com>
2026-07-20 03:10:06 +0300

    Card.ts

---
+++ src/components/view/Card.ts
@@ -0,0 +1,21 @@
+import { Component } from '../base/Component';
+import { IProduct } from '../../types';
+
+export class Card extends Component<IProduct> {
+    protected titleElement: HTMLElement;
+    protected priceElement: HTMLElement;
+
+    constructor(container: HTMLElement) {
+        super(container);
+        this.titleElement = container.querySelector('.card__title') as HTMLElement;
+        this.priceElement = container.querySelector('.card__price') as HTMLElement;
+    }
+
+    set title(value: string) {
+        this.setText(this.titleElement, value);
+    }
+
+    set price(value: number | null) {
+        this.setText(this.priceElement, value ? `${value} синапсов` : 'Бесценно');
+    }
+}
