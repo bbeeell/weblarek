172063e26ed3013f82280a933665b704d936521d
why_bbbelll <borlakovabella326@gmail.com>
2026-07-20 11:22:50 +0300

    OrderForm.ts

--- src/components/view/OrderForm.ts
+++ src/components/view/OrderForm.ts
@@ -3,14 +3,15 @@ import { IOrderForm } from '../../types';
 
 export class OrderForm extends Form<IOrderForm> {
     protected paymentButtons: HTMLButtonElement[];
+    protected addressInput: HTMLInputElement;
 
     constructor(container: HTMLFormElement, events: IEvents) {
         super(container, events);
         this.paymentButtons = Array.from(container.querySelectorAll('button[name="payment"]'));
+        this.addressInput = container.querySelector('input[name="address"]') as HTMLInputElement;
         
         this.paymentButtons.forEach(button => {
             button.addEventListener('click', () => {
-                this.payment = button.name;
                 this.events.emit('order:changed', { field: 'payment', value: button.name });
             });
         });
@@ -23,6 +24,6 @@ export class OrderForm extends Form<IOrderForm> {
     }
 
     set address(value: string) {
-        (this.container.querySelector('input[name="address"]') as HTMLInputElement).value = value;
+        this.addressInput.value = value;
     }
 }
