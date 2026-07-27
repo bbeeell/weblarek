1f01ff9669224b47f37ac87414d9fb125ce20446
why_bbbelll <borlakovabella326@gmail.com>
2026-07-27 18:53:50 +0300

    Basket.ts

---
+++ src/components/view/Basket.ts
@@ -0,0 +1,34 @@
+import { Component } from '../base/Component';
+import { IEvents } from '../base/Events';
+
+export class Basket extends Component<HTMLElement> {
+    protected listElement: HTMLElement;
+    protected totalElement: HTMLElement;
+    protected orderButton: HTMLButtonElement;
+
+    constructor(container: HTMLElement, protected events: IEvents) {
+        super(container);
+        this.listElement = container.querySelector('.basket__list') as HTMLElement;
+        this.totalElement = container.querySelector('.basket__price') as HTMLElement;
+        this.orderButton = container.querySelector('.basket__button') as HTMLButtonElement;
+
+        // Кнопка сразу неактивна (защита от эмитов)
+        this.setDisabled(this.orderButton, true);
+
+        this.orderButton.addEventListener('click', () => this.events.emit('order:open'));
+    }
+
+    set items(items: HTMLElement[]) {
+        if (items.length === 0) {
+            this.listElement.innerHTML = '<p>Корзина пуста</p>';
+            this.setDisabled(this.orderButton, true);
+        } else {
+            this.listElement.replaceChildren(...items);
+            this.setDisabled(this.orderButton, false);
+        }
+    }
+
+    set total(value: number) {
+        this.setText(this.totalElement, `${value} синапсов`);
+    }
+}
