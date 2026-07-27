33c3cde43968a8be15d7352178fe650a904c048f
why_bbbelll <borlakovabella326@gmail.com>
2026-07-27 18:51:36 +0300

    CardBasket.ts

--- src/components/view/CardBasket.ts
+++ src/components/view/CardBasket.ts
@@ -1,18 +1,14 @@
-import { Component } from '../base/Component';
+import { Card } from './Card';
 import { IEvents } from '../base/Events';
 
-export class CardBasket extends Component<HTMLElement> {
+export class CardBasket extends Card {
     protected indexElement: HTMLElement;
     protected deleteButton: HTMLButtonElement;
-    protected titleElement: HTMLElement;
-    protected priceElement: HTMLElement;
 
     constructor(container: HTMLElement, protected events: IEvents, itemId: string) {
         super(container);
         this.indexElement = container.querySelector('.basket__item-index') as HTMLElement;
         this.deleteButton = container.querySelector('.basket__item-delete') as HTMLButtonElement;
-        this.titleElement = container.querySelector('.card__title') as HTMLElement;
-        this.priceElement = container.querySelector('.card__price') as HTMLElement;
 
         this.deleteButton.addEventListener('click', () => {
             this.events.emit('basket:remove', { id: itemId });
@@ -21,13 +17,5 @@ export class CardBasket extends Component<HTMLElement> {
 
     set index(value: number) {
         this.setText(this.indexElement, String(value));
-    }
-
-    set title(value: string) {
-        this.setText(this.titleElement, value);
-    }
-
-    set price(value: number | null) {
-        this.setText(this.priceElement, value !== null ? `${value} синапсов` : 'Бесценно');
     }
 }
